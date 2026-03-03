664f79b9d3a69a70f8cc7e24181d64ba1288e8bd
MALISMF <matveyfox.mf@gmail.com>
2026-03-03 19:52:58 +0800

    add car_control

---
+++ car_control/car_control.ino
@@ -0,0 +1,60 @@
+#define DIR_LEFT 4
+#define SPEED_LEFT 5
+
+#define DIR_RIGHT 7
+#define SPEED_RIGHT 6
+
+#define FORWARD_LEFT LOW
+#define BACKWARD_LEFT HIGH
+
+#define FORWARD_RIGHT HIGH
+#define BACKWARD_RIGHT LOW
+
+void move(bool left_dir, int left_speed, bool right_dir, int right_speed) {
+  digitalWrite(DIR_LEFT, left_dir);
+  digitalWrite(DIR_RIGHT, right_dir);
+  analogWrite(SPEED_LEFT, left_speed);
+  analogWrite(SPEED_RIGHT, right_speed);
+
+}
+
+void forward(int speed) {
+  move(FORWARD_LEFT, speed, FORWARD_RIGHT, speed);
+}
+
+void backward(int speed) {
+  move(BACKWARD_LEFT, speed, BACKWARD_RIGHT, speed);
+}
+
+void turn_left(int steepness) {
+  // steepness is how steep is turn
+  //
+
+}
+
+void turn_right(int steepness) {}
+
+
+void rotate_left(int speed) {}
+
+void rotate_right(int speed) {}
+
+
+void setup() {
+  pinMode(DIR_LEFT, OUTPUT);
+  pinMode(DIR_RIGHT, OUTPUT);
+  pinMode(SPEED_LEFT, OUTPUT);
+  pinMode(SPEED_RIGHT, OUTPUT);
+  move(FORWARD_LEFT, 255, FORWARD_RIGHT, 0);
+  delay(2000);
+  move(FORWARD_LEFT, 0, FORWARD_RIGHT, 255);
+  delay(2000);
+  move(FORWARD_LEFT, 0, FORWARD_RIGHT, 0);
+}
+
+
+void loop() {
+
+}
+
+
